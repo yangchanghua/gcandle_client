537cb73c5387c9073f6a3fced7305b41dd11ab95
ricyang <yangchanghua@gmail.com>
2020-09-23 22:26:59 +0800

    with default buy strategy, simpler buy.

--- src/gcandle_client/gclient_ma_cross/ma_cross_backtest.py
+++ src/gcandle_client/gclient_ma_cross/ma_cross_backtest.py
@@ -1,12 +1,21 @@
 from gcandle.core.backtest.day_bar_backtest import DayBarBacktest
-from gcandle.core.buy_strategy import BuyStrategy
+from gcandle.core.buy_strategy.BuyStrategy import BuyStrategy
 from gcandle.core.sell_strategy.CombinedSellStrategy import CombinedSellStrategy
 from gcandle.core.sell_strategy.SellAtCloseStrategy import SellAtCloseStrategy
 from gcandle_client.gclient_ma_cross.ma_cross import MA_CROSS_SERVICE
-from gcandle_client.gclient_ma_cross.ma_cross_buy import MaCrossBuy
 
 
-MY_BUY_STRATEGY = MaCrossBuy()
+def final_buy_filter_on_buy_day(data):
+    return data.ma_jc == True
+
+
+def my_price_func(data):
+    return data.close
+
+
+MY_BUY_STRATEGY = BuyStrategy('ma cross').\
+    set_filter_on_buy_day(final_buy_filter_on_buy_day).\
+    set_price_func(my_price_func)
 
 
 MY_SELL_STRATEGY = CombinedSellStrategy("ma cross").append(
